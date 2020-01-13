183d6aeb05a7decd3482e16b6ccf1837204cefad
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-13 11:43:26 -0800

    Added dotenv dependancy.

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+require('dotenv').config();
+
+app.listen(PORT, () => console.log('do things'));
